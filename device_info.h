ce2dacf0934b77a3b6add1b9541d8adca40b9a00
216k155 <216k155@luxcore.io>
2018-02-21 11:14:28 +1100

    Updated

---
+++ device_info.h
@@ -0,0 +1,12 @@
+//
+// Created by 216k155 on 2/20/18.
+//
+
+#ifndef CMAKE_AND_CUDA_DEVICE_INFO_H
+#define CMAKE_AND_CUDA_DEVICE_INFO_H
+
+
+void printDeviceProps();
+
+
+#endif //CMAKE_AND_CUDA_DEVICE_INFO_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 216k155 on 2/20/18.
+//
+#include "device_info.h"
+
+
+int main(int argc, char ** argv) {
+    printDeviceProps();
+}
