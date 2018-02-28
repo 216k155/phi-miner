fe6b13cc012337a6e36b0a973bdf64e3e96bd835
216k155 <216k155@luxcore.io>
2018-03-01 00:43:06 +1100

    Qt+Cuda+Cmake initial app + mining dialog firsdt integrated

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 //
-// Created by 216k155 on 2/20/18.
+// Created by 216k155
 //
 #include "device_info.h"
+#include <qapplication.h>
+#include <qpushbutton.h>
 
 
-int main(int argc, char ** argv) {
+int main( int argc, char **argv )
+{
     printDeviceProps();
+    QApplication a( argc, argv );
+
+    QPushButton hello( "Hello world!", 0 );
+    hello.resize( 100, 30 );
+
+   // a.setMainWidget( &hello );
+    hello.show();
+    return a.exec();
 }
---
+++ miningdialog.h
@@ -0,0 +1,51 @@
+//
+// Created by 216k155
+//
+
+#ifndef CMAKE_AND_CUDA_MININGDIALOG_H
+#define CMAKE_AND_CUDA_MININGDIALOG_H
+
+#include <QMainWindow>
+#include <QProcess>
+#include <QTextBrowser>
+#include <QString>
+#include <QListIterator>
+#include <QTextCharFormat>
+
+
+namespace Ui
+{
+    class MiningDialog;
+}
+
+QT_BEGIN_NAMESPACE
+class QModelIndex;
+QT_END_NAMESPACE
+
+/** Dialog showing transaction details. */
+class MiningDialog : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MiningDialog(QWidget* parent = 0);
+    void closeEvent (QCloseEvent *event);
+    void setTextTermFormatting(QTextBrowser * textEdit, QString const & text);
+    void parseEscapeSequence(int attribute, QListIterator< QString > & i, QTextCharFormat & textCharFormat, QTextCharFormat const & defaultTextCharFormat);
+    ~MiningDialog();
+
+private:
+    Ui::MiningDialog* ui;
+    bool m_NeverShown;
+    int m_HistoryIndex;
+    QStringList m_History;
+    QProcess *minerLogProcess = NULL;
+private Q_SLOTS:
+    void run_benchmark();
+    void run_mining();
+    void stop_mining();
+    void ReadOut();
+    void ReadErr();
+};
+
+#endif //CMAKE_AND_CUDA_MININGDIALOG_H
